c869d7b0437ef9ef730fe66670c57911ca36bbe1
Arjun Ghimire <Arjunghimire0714@gmail.com>
2016-10-17 15:37:05 +0545

    Calling class method inside class

---
+++ classmethod.rb
@@ -0,0 +1,14 @@
+# calling class method inside class
+class Person
+	def self.name
+		puts "I am Name Method"
+	end
+
+	def age
+		Person.name # this code call self.name method 
+		puts "I am from age Method"
+	end
+end
+
+obj = Person.new 
+obj.age
