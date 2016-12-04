856f9528266a40b500c8d2f8c3b8ea43fd695dc7
Bishnu Basyal <imvsnu@gmail.com>
2016-12-04 16:40:42 +0545

    lambda

--- lambda.rb
+++ lambda.rb
@@ -1,19 +1,31 @@
-    def try_proc
-      x = Proc.new {
-        return # Return from try_proc
-      }
-      x.call
-      puts "After x.call" # this line is never reached
+=begin
+def try_proc
+  x = Proc.new {
+    return # Return from try_proc
+  }
+  x.call
+  puts "After x.call" # this line is never reached
+end
+
+def try_lambda
+  y = -> {
+    return # return from y
+  }
+  y.call
+  puts "After y.call" # this line is not skipped
+end
+
+try_proc # No output
+try_lambda # Outputs "After y.call"
+=end
+class Greeter
+  def to_proc
+    Proc.new do |item|
+      puts "Hello, #{item}"
     end
+  end
+end
 
-    def try_lambda
-      y = -> {
-        return # return from y
-      }
-      y.call
-      puts "After y.call" # this line is not skipped
-    end
+greet = Greeter.new
 
-    try_proc # No output
-    try_lambda # Outputs "After y.call"
-
+%w(world life).each(&greet)
